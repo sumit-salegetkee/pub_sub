15398dd608f0c20a13adda169c0d43135929c441
Sumit Sale <sumit.sale@getkee.com>
2021-01-23 14:29:56 +0530

    added try catch

--- server.js
+++ server.js
@@ -22,11 +22,21 @@ var device = awsIot.device({
 device
   .on('connect', function() {
     console.log('connect');
-    //device.subscribe('topic_1');
-    device.publish('topic_2', JSON.stringify({ test_data: 'NodeJS server connected...'}));
+    device.subscribe('topic_2');
+    try{
+      device.publish('topic_2', JSON.stringify({ test_data: 'NodeJS server connected...'}));  
+    } catch(err){
+      console.log(err);
+    }
+    
   });
  
 device
   .on('message', function(topic, payload) {
-    console.log('message', topic, payload.toString());
+    try{
+      console.log('message', topic, payload.toString());  
+    } catch(err){
+      console.log(err);
+    }
+    
   });
